512c0049b78ecf1cee6ba68b666e5893591d3fd1
Cody Scott <cody.j.b.scott@gmail.com>
2017-01-05 12:16:24 -0500

    pyqtProperty with list

---
+++ App.qml
@@ -0,0 +1,19 @@
+import QtQuick 2.7
+import QtQuick.Controls 1.4
+import QtQuick.Layouts 1.3
+
+import CustomType 1.0
+
+ApplicationWindow {
+    id: appWindow
+    objectName: "appWindow"
+    title: "Custom type with list property"
+    visible: true
+    width: 800
+    height: 600
+
+    CustomType {
+      id: customType
+      names: []
+    }
+}
---
+++ app.py
@@ -0,0 +1,35 @@
+import sys
+
+from PyQt5 import QtCore, QtQml
+from PyQt5.QtWidgets import QApplication
+
+
+class CustomType(QtCore.QObject):
+    names_changed = QtCore.pyqtSignal()
+
+    def __init__(self, parent = None):
+        super(CustomType, self).__init__(parent)
+
+        self._names = []
+
+    @QtCore.pyqtProperty(list, notify = names_changed)
+    def names(self):
+        return self._names
+    @names.setter
+    def names(self, names):
+        print('here1')
+        if names == self.names:
+            print('here2')
+            return
+        print('here3')
+        self._names = names
+        print('here4')
+        self.names_changed.emit()
+        print('here5')
+        print('============\n')
+
+
+app = QApplication(sys.argv)
+QtQml.qmlRegisterType(CustomType, 'CustomType', 1, 0, 'CustomType')
+engine = QtQml.QQmlApplicationEngine("App.qml")
+app.exec_()
